df312ed613bc12ffffaf70479281ea68fc0c64d8
Blue <leanefelis@gmail.com>
2021-11-06 17:18:28 +0100

    changement du site vers une autre version

---
+++ go_info/script.js
@@ -0,0 +1,82 @@
+/* ❤❤❤ */
+// Img gallery
+
+const imgContent = document.querySelectorAll('.img-content-hover');
+
+function showImgContent(e) {
+  for (var i = 0; i < imgContent.length; i++) {
+    x = e.pageX;
+    y = e.pageY;
+    imgContent[i].style.transform = `translate3d(${x}px, ${y}px, 0)`;
+  }
+};
+
+
+document.addEventListener('mousemove', showImgContent);
+
+// Fin img gallery
+(function ($) {
+  "use strict";
+
+  //Switch dark/light
+
+  $(".switch").on("click", function () {
+    if ($("body").hasClass("light")) {
+      $("body").removeClass("light");
+      $(".switch").removeClass("switched");
+    } else {
+      $("body").addClass("light");
+      $(".switch").addClass("switched");
+    }
+  });
+
+  $(document).ready(function () {
+    var d = new Date();
+    var n = d.getHours();
+  
+    // entre 17h et 8h le thème sombre sera désactivé
+    if (n > 8 || n < 17) {
+      $("body").addClass("light");
+      $(".switch").addClass("switched");
+    }
+  });
+
+  $(document).ready(function () {
+    "use strict";
+
+    //Scroll back to top
+
+    var progressPath = document.querySelector(".progress-wrap path");
+    var pathLength = progressPath.getTotalLength();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "none";
+    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
+    progressPath.style.strokeDashoffset = pathLength;
+    progressPath.getBoundingClientRect();
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+      "stroke-dashoffset 10ms linear";
+    var updateProgress = function () {
+      var scroll = $(window).scrollTop();
+      var height = $(document).height() - $(window).height();
+      var progress = pathLength - (scroll * pathLength) / height;
+      progressPath.style.strokeDashoffset = progress;
+    };
+    updateProgress();
+    $(window).scroll(updateProgress);
+    var offset = 50;
+    var duration = 550;
+    jQuery(window).on("scroll", function () {
+      if (jQuery(this).scrollTop() > offset) {
+        jQuery(".progress-wrap").addClass("active-progress");
+      } else {
+        jQuery(".progress-wrap").removeClass("active-progress");
+      }
+    });
+    jQuery(".progress-wrap").on("click", function (event) {
+      event.preventDefault();
+      jQuery("html, body").animate({ scrollTop: 0 }, duration);
+      return false;
+    });
+  });
+
+})(jQuery);
